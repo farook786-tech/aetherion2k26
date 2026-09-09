8fdd2e4c337db76bf40a19814a28de65f10373c2
mazen maz <sharafahshahul@gmail.com>
2026-09-09 17:23:42 +0530

    Fix missing useState import in event-explorer causing runtime crash

--- components/event-explorer.tsx
+++ components/event-explorer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { EventItem, EVENTS_DATA } from '@/lib/data/events';
 import EventModal from './event-modal';
